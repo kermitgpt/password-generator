9a8a49fb1f7632e22aad32439d48ba50bf645763
Matt Grogan <mattgrogan.dev@gmail.com>
2021-11-07 00:16:02 -0700

    added more to gen pw function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,9 @@
-/* var pwLength = prompt(
-  "Please pick a number between 8 and 128 for password length."
-); */
+var options;
 
-var random = {
-  lower: getLower,
-  upper: getUpper,
-  number: getNum,
-  special: getSpecial,
-};
+var pwLower;
+var pwUpper;
+var pwNum;
+var pwSpecial;
 
 var pwLength = function () {
   var userInput = prompt(
@@ -20,8 +16,6 @@ var pwLength = function () {
     return userInput;
   }
 };
-
-pwLength();
 
 function getLower() {
   if (pwLower == true) {
@@ -53,46 +47,38 @@ function getSpecial() {
   }
 }
 
-var pwLower = confirm("Do you want to add lower case characters?");
-
-var pwUpper = confirm("Do you want to add upper case characters?");
-
-var pwNum = confirm("Do you want to add numeric characters?");
-
-var pwSpecial = confirm("Do you want to add special characters?");
-
-console.log(getLower());
-console.log(getUpper());
-console.log(getNum());
-console.log(getSpecial());
-
-getLower();
-
-/* var pwUpper = confirm("Do you want to add upper case characters?");
-
-var pwNum = confirm("Do you want to add numeric characters?");
-
-var pwSpecial = confirm("Do you want to add special characters?"); */
-
 //Start pw generation, filter out checked & unchecked regarding upper/lower/number/special.
 //Loop over length for each function that was checked
 //Adds final password after doing all of this
 
-/* 
-function generatePassword(lower, upper, number, special, length) {
-  let generatePassword = "";
+function generatePassword() {
+  pwLength();
 
-  const typeCount = lower + upper + number + special;
-  console.log(typeCount);
+  var pwLower = confirm("Do you want to add lower case characters?");
 
-  const typeArray = [{ lower }, { upper }, { number }, { symbol }].filter(
-    (typeArray) => Object.values(typeArray)[0]
-  );
+  var pwUpper = confirm("Do you want to add upper case characters?");
 
-  console.log(typeArray);
+  var pwNum = confirm("Do you want to add numeric characters?");
+
+  var pwSpecial = confirm("Do you want to add special characters?");
+
+  if (!pwLower && !pwUpper && !pwNum && !pwSpecial) {
+    alert(
+      "You must choose one of the four options! Lower, Upper, Number, or Special."
+    );
+    var pwLower = confirm("Do you want to add lower case characters?");
+
+    var pwUpper = confirm("Do you want to add upper case characters?");
+
+    var pwNum = confirm("Do you want to add numeric characters?");
+
+    var pwSpecial = confirm("Do you want to add special characters?");
+  }
 }
 
-console.log(generatePassword()); */
+generatePassword();
+
+/* generatePassword(); */
 
 //Driver function. Check each condition, generate some sort of for loop,
 
@@ -113,3 +99,88 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //use length of the array, in the same way math.random on the array of get lower, get num, get special. Use
+
+/* var pwLength = prompt(
+  "Please pick a number between 8 and 128 for password length."
+); */
+
+/* var random = {
+  lower: getLower,
+  upper: getUpper,
+  number: getNum,
+  special: getSpecial,
+}; */
+
+/* const random = function (generate) {
+  const lower = getLower;
+  const upper = getUpper;
+  const number = getNum;
+  const special = getSpecial;
+
+  result (lower, upper, number, special)
+}; */
+
+/* var pwLower = confirm("Do you want to add lower case characters?");
+
+var pwUpper = confirm("Do you want to add upper case characters?");
+
+var pwNum = confirm("Do you want to add numeric characters?");
+
+var pwSpecial = confirm("Do you want to add special characters?"); */
+
+/* function getLower() {
+  var pwLower = confirm("Do you want to add lower case characters?");
+  if (pwLower == true) {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    console.log(getLower());
+  } else {
+    return false;
+  }
+}
+function getUpper() {
+  var pwUpper = confirm("Do you want to add upper case characters?");
+  if (pwUpper == true) {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    console.log(getUpper());
+  } else {
+    return false;
+  }
+}
+function getNum() {
+  var pwNum = confirm("Do you want to add numeric characters?");
+  if (pwNum == true) {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    console.log(getNum);
+  } else {
+    return false;
+  }
+}
+function getSpecial() {
+  var pwSpecial = confirm("Do you want to add special characters?");
+  if (pwSpecial == true) {
+    var symbols = "!@#$%^&*()[]{}=<>/,.";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+    console.log(getSpecial());
+  } else {
+    return false;
+  }
+}
+
+getLower("");
+getUpper("");
+getNum("");
+getSpecial("");
+ */
+
+/* var pwLength = function () {
+  var enter = parseInt(
+    prompt("Please pick a number between 8 and 128 for password length.")
+  );
+  if (enter < 8 || enter > 128) {
+    pwLength();
+  } else {
+    return parseInt(
+      prompt("Please pick a number between 8 and 128 for password length.")
+    );
+  }
+}; */
